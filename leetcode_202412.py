3a0accefc643fad1b3e28cd284d20f414540ac67
Nguyen Pham <phamhoannguyen@gmail.com>
2024-12-04 19:15:24 +0700

    LeetCode 225

--- leetcode_202412.py
+++ leetcode_202412.py
@@ -1,3 +1,6 @@
+import functools  # noqa: F401
+
+
 def addSpaces2109(s: str, spaces: list[int]) -> str:
     positions = [
         {"end": space_position, "start": spaces[index - 1] if index else 0}
@@ -7,6 +10,7 @@ def addSpaces2109(s: str, spaces: list[int]) -> str:
 
 
 # print(addSpaces2109("EnjoyYourCoffee", [5, 9]))
+
 
 def minEnd3133(n: int, x: int) -> int:
     """
@@ -29,12 +33,12 @@ def minEnd3133(n: int, x: int) -> int:
     """
     zero_count = bin(x).count("0") - 1
     if zero_count == 0:
-        return ((n-1) << x.bit_length()) | x
-    n1 = (n-1) // (2**zero_count)
-    n2 = (n-1) % (2**zero_count)
+        return ((n - 1) << x.bit_length()) | x
+    n1 = (n - 1) // (2**zero_count)
+    n2 = (n - 1) % (2**zero_count)
     list_2 = list(bin(x)[2:])
     n2_reversed_number = reversed(bin(n2)[2:])
-    for i in range(len(list_2)-1, 0, -1):
+    for i in range(len(list_2) - 1, 0, -1):
         try:
             if list_2[i] == "0":
                 list_2[i] = next(n2_reversed_number)
@@ -43,7 +47,32 @@ def minEnd3133(n: int, x: int) -> int:
     # return int(f"{bin(n1)[2:]}{''.join(list_2)}",2)
     return f"{bin(n1)[2:]}{''.join(list_2)}"
 
+
 # print(minEnd3133(6715154, 7193485))
 # print(minEnd3133(4, 1))
 # print(minEnd3133(3, 4))
 # print(minEnd3133(2, 2))
+
+
+def largestCombination2275(candidates: list[int]) -> int:
+    """
+      10000
+      10001
+    1000111
+     111110
+       1100
+      11000
+       1110
+    -------
+    1144432
+    """
+    largest = 0
+    for i in range(max(candidates).bit_length()):
+        largest = max(largest, sum([1 for number in candidates if number & (1 << i)]))
+        # largest = max(largest, len(list(filter(lambda number: number & (1 << i), candidates))))
+        # largest = max(largest, functools.reduce(lambda cnt, number: cnt +1 if number & (1 << i) else cnt, candidates, 0))
+    return largest
+
+
+print(largestCombination2275([16, 17, 71, 62, 12, 24, 14]))
+print(largestCombination2275([8, 8]))
