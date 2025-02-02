f08b9f84f7ff0a9498e7f97a9b094e736b655419
Nguyen Pham <phamhoannguyen@gmail.com>
2025-02-02 17:36:22 +0700

    1752

--- leetcode_202502.go
+++ leetcode_202502.go
@@ -19,8 +19,74 @@ func isArraySpecial3151(nums []int) bool {
 	return true
 }
 
+func findRedundantConnection648(edges [][]int) []int {
+	// 648
+	//         3
+	//         |
+	// 1 - 5 - 8 - 6 - 2
+	//         |
+	//        10 - 9 - 4
+	//             |
+	//             7
+	// {4, 10}
+	nodeMap := make([]int, 11) // n <= 1000
+	errors := [][]int{}
+	for _, edge := range edges {
+		if (nodeMap[edge[0]] == 1) && (nodeMap[edge[1]] == 1) {
+			errors = append(errors, edge)
+		} else {
+			nodeMap[edge[0]] = 1
+			nodeMap[edge[1]] = 1
+		}
+		fmt.Println(nodeMap, errors)
+	}
+	return errors[len(errors)-1]
+}
+
+func check1752_fail(nums []int) bool {
+	// 1752
+	maxNum, wrappedNum := 0, 101 // nums[i] <= 100
+	for i := 1; i < len(nums); i++ {
+		if nums[i] < nums[i-1] {
+			if wrappedNum != 101 {
+				return false
+			} else {
+				wrappedNum = nums[i-1]
+			}
+		}
+		if nums[i] > maxNum {
+			maxNum = nums[i]
+		}
+		if nums[i] > wrappedNum {
+			return false
+		}
+	}
+	return true
+}
+
+func check1752(nums []int) bool {
+	// 1752
+	wrappedOnce := false
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > nums[(i+1)%len(nums)] {
+			if wrappedOnce {
+				return false
+			} else {
+				wrappedOnce = true
+			}
+		}
+	}
+	return true
+}
+
 func main() {
-	fmt.Println(isArraySpecial3151([]int{1, 6, 2}))    // false
-	fmt.Println(isArraySpecial3151([]int{2, 1, 4}))    // true
-	fmt.Println(isArraySpecial3151([]int{4, 3, 1, 6})) // false
+	// fmt.Println(check1752([]int{2, 4, 1, 3}))    // false
+	// fmt.Println(check1752([]int{2, 1, 3, 4}))    // false
+	// fmt.Println(check1752([]int{3, 4, 5, 1, 2})) // true
+	// fmt.Println(check1752([]int{6, 10, 6}))      // true
+	fmt.Println(findRedundantConnection648([][]int{{9, 10}, {5, 8}, {2, 6}, {1, 5}, {3, 8}, {4, 9}, {8, 10}, {4, 10}, {6, 8}, {7, 9}}))
+	fmt.Println(findRedundantConnection648([][]int{{1, 2}, {2, 3}, {3, 4}, {1, 4}, {1, 5}}))
+	// fmt.Println(isArraySpecial3151([]int{1, 6, 2}))    // false
+	// fmt.Println(isArraySpecial3151([]int{2, 1, 4}))    // true
+	// fmt.Println(isArraySpecial3151([]int{4, 3, 1, 6})) // false
 }
