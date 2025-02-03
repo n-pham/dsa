6679d9b8befcee90c3e0b1da98cdeec1fc342401
Nguyen Pham <phamhoannguyen@gmail.com>
2025-02-03 15:16:57 +0700

    3105

--- leetcode_202502.go
+++ leetcode_202502.go
@@ -19,7 +19,7 @@ func isArraySpecial3151(nums []int) bool {
 	return true
 }
 
-func findRedundantConnection648(edges [][]int) []int {
+func findRedundantConnection648_fail(edges [][]int) []int {
 	// 648
 	//         3
 	//         |
@@ -41,6 +41,47 @@ func findRedundantConnection648(edges [][]int) []int {
 		fmt.Println(nodeMap, errors)
 	}
 	return errors[len(errors)-1]
+}
+
+func findRedundantConnection648(edges [][]int) []int {
+	// 648
+	//         3
+	//         |
+	// 1 - 5 - 8 - 6 - 2
+	//         |
+	//        10 - 9 - 4
+	//             |
+	//             7
+	// {4, 10}
+	parent := make([]int, len(edges)+1)
+	for i := range parent {
+		parent[i] = i
+	}
+
+	var find func(int) int
+	find = func(x int) int {
+		if parent[x] != x {
+			parent[x] = find(parent[x])
+		}
+		return parent[x]
+	}
+
+	union := func(x, y int) bool {
+		rootX := find(x)
+		rootY := find(y)
+		if rootX != rootY {
+			parent[rootX] = rootY
+			return true
+		}
+		return false
+	}
+
+	for _, edge := range edges {
+		if !union(edge[0], edge[1]) {
+			return edge
+		}
+	}
+	return nil
 }
 
 func check1752_fail(nums []int) bool {
@@ -79,13 +120,36 @@ func check1752(nums []int) bool {
 	return true
 }
 
+func longestMonotonicSubarray3105(nums []int) int {
+	// 3105
+	maxLen, prev, incLen, decLen := 1, nums[0], 1, 1
+	for _, num := range nums[1:] {
+		if num > prev {
+			decLen, incLen = 1, incLen+1
+			if incLen > maxLen {
+				maxLen = incLen
+			}
+		} else if prev > num {
+			decLen, incLen = decLen+1, 1
+			if decLen > maxLen {
+				maxLen = decLen
+			}
+		} else {
+			decLen, incLen = 1, 1
+		}
+		prev = num
+	}
+	return maxLen
+}
+
 func main() {
+	fmt.Println(longestMonotonicSubarray3105([]int{1, 4, 3, 3, 2}))
 	// fmt.Println(check1752([]int{2, 4, 1, 3}))    // false
 	// fmt.Println(check1752([]int{2, 1, 3, 4}))    // false
 	// fmt.Println(check1752([]int{3, 4, 5, 1, 2})) // true
 	// fmt.Println(check1752([]int{6, 10, 6}))      // true
-	fmt.Println(findRedundantConnection648([][]int{{9, 10}, {5, 8}, {2, 6}, {1, 5}, {3, 8}, {4, 9}, {8, 10}, {4, 10}, {6, 8}, {7, 9}}))
-	fmt.Println(findRedundantConnection648([][]int{{1, 2}, {2, 3}, {3, 4}, {1, 4}, {1, 5}}))
+	// fmt.Println(findRedundantConnection648([][]int{{9, 10}, {5, 8}, {2, 6}, {1, 5}, {3, 8}, {4, 9}, {8, 10}, {4, 10}, {6, 8}, {7, 9}}))
+	// fmt.Println(findRedundantConnection648([][]int{{1, 2}, {2, 3}, {3, 4}, {1, 4}, {1, 5}}))
 	// fmt.Println(isArraySpecial3151([]int{1, 6, 2}))    // false
 	// fmt.Println(isArraySpecial3151([]int{2, 1, 4}))    // true
 	// fmt.Println(isArraySpecial3151([]int{4, 3, 1, 6})) // false
