fcfd1f951c8b75fb95375f89d69e79b42295fe7e
Nguyen Pham <phamhoannguyen@gmail.com>
2025-02-14 14:53:49 +0700

    1352

--- leetcode_202502.go
+++ leetcode_202502.go
@@ -1046,9 +1046,48 @@ func minOperations3066(nums []int, k int) int {
 	return operations
 }
 
+type ProductOfNumbers struct {
+	products []int
+}
+
+
+func Constructor() ProductOfNumbers {
+	return ProductOfNumbers{products: []int{1}}
+}
+
+
+func (this *ProductOfNumbers) Add(num int) {
+	if num == 0 {
+		this.products = []int{1}
+	} else {
+		this.products = append(this.products, this.products[len(this.products)-1]*num)
+	}
+}
+
+
+func (this *ProductOfNumbers) GetProduct(k int) int {
+	n := len(this.products)
+	if k >= n {
+		return 0
+	}
+	return this.products[n-1] / this.products[n-1-k]
+}
+
+
 func main() {
+	ProductOfNumbers productOfNumbers = new ProductOfNumbers()
+	productOfNumbers.add(3)        // [3]
+	productOfNumbers.add(0)        // [3,0]
+	productOfNumbers.add(2)        // [3,0,2]
+	productOfNumbers.add(5)        // [3,0,2,5]
+	productOfNumbers.add(4)        // [3,0,2,5,4]
+	productOfNumbers.getProduct(2) // return 20. The product of the last 2 numbers is 5 * 4 = 20
+	productOfNumbers.getProduct(3) // return 40. The product of the last 3 numbers is 2 * 5 * 4 = 40
+	productOfNumbers.getProduct(4) // return 0. The product of the last 4 numbers is 0 * 2 * 5 * 4 = 0
+	productOfNumbers.add(8)        // [3,0,2,5,4,8]
+	productOfNumbers.getProduct(2) // return 32. The product of the last 2 numbers is 4 * 8 = 32 
 	// fmt.Println('0'-'A', '9'-'A', 'z'-'0', 'a'-'A', 'Z'-'A')
-	fmt.Println(productExceptSelf238([]int{1, 2, 3, 4}))
+	// fmt.Println(productExceptSelf238([]int{1, 2, 3, 4}))
 	// fmt.Println(isValidSudoku36([][]byte{
 	// 	{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
 	// 	{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
