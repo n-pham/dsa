930201ceea78401634ac30cc1781e519dbd4b984
Nguyen Pham <phamhoannguyen@gmail.com>
2025-03-09 08:12:48 +0700

    2352 3208 in progress

--- leetcode_202503.go
+++ leetcode_202503.go
@@ -435,6 +435,27 @@ func singleNumber_solution(nums []int) []int {
     return []int{num1, num2}
 }
 
+func equalPairs(grid [][]int) int {
+	// 2352
+	// 3,1,2,2
+	// 1,4,4,5
+	// 2,4,2,2
+	// 2,4,2,2
+	panic("not implemented")
+}
+
+func numberOfAlternatingGroups(colors []int, k int) int {
+	// 3208
+	// 0,1,0,0,1,0,1    6
+	// 0 1 0   1 0 1
+	// 0 1   0 1 0 1
+	// 0,1,0,1,0        3
+	// 0 1 0
+	//   1 0 1
+	//     0 1 0
+	panic("not implemented")
+}
+
 func main() {
 	fmt.Println(singleNumber([]int{1,2,1,3,2,5}))
 	// fmt.Println(countSubstrings("fdsklf"))
