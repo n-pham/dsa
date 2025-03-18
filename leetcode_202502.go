569da022b3271b06ff29ac49753016da9871b0b4
Nguyen Pham <phamhoannguyen@gmail.com>
2025-03-18 10:51:52 +0700

    1092 in progress

--- leetcode_202502.go
+++ leetcode_202502.go
@@ -1793,16 +1793,18 @@ func maxAbsoluteSum1749(nums []int) int {
 }
 
 // tried
-//    1,2,3,4,5,6,7,8
-//    1 2 3   5     8
-//    1   3 4     7
-//    1       5 6
-//      2   4   6
-//    1         6 7
-//      2     5   7
-//    1           7 8
-//      2       6   8
-//        3   5     8
+//
+//	1,2,3,4,5,6,7,8
+//	1 2 3   5     8
+//	1   3 4     7
+//	1       5 6
+//	  2   4   6
+//	1         6 7
+//	  2     5   7
+//	1           7 8
+//	  2       6   8
+//	    3   5     8
+//
 // 1    1
 // 2    1, 2, 3=1+2
 // 3    1, 2, 3, 4=3+1, 5=3+2
@@ -1839,6 +1841,14 @@ func lenLongestFibSubseq873_fail(arr []int) int {
 		return maxLen
 	}
 	return 0
+}
+
+func shortestCommonSupersequence1092(str1 string, str2 string) string {
+	// 1092
+	//  abac cab
+	// cab
+	// cabac
+	panic("not implemented")
 }
 
 func main() {
