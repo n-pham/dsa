f4d7a3349269b8ca996635abb6b53b26a00bd483
Nguyen Pham <phamhoannguyen@gmail.com>
2025-04-02 17:54:10 +0700

    2908

--- lc202504/main.go
+++ lc202504/main.go
@@ -87,11 +87,6 @@ func maximumTripletValue(nums []int) int64 {
 	return max
 }
 
-func minimumSum(nums []int) int {
-	// 2908
-	panic("not implemented")
-}
-
 func arithmeticTriplets(nums []int, diff int) (rs int) {
 	// 2367
 	// i-j j-k
@@ -109,9 +104,38 @@ func arithmeticTriplets(nums []int, diff int) (rs int) {
 	return rs
 }
 
+func minimumSum(nums []int) int {
+	// 2908
+	rs := math.MaxInt
+	for j := 1; j < len(nums)-1; j++ {
+		for i := 0; i < j; i++ {
+			if nums[i] < nums[j] {
+				kMin := math.MaxInt
+				for k := j + 1; k < len(nums); k++ {
+					if nums[j] > nums[k] && kMin > nums[k] {
+						kMin = nums[k]
+					}
+				}
+				fmt.Println(nums[i], nums[j], kMin)
+				if kMin != math.MaxInt {
+					tmp := nums[i] + nums[j] + kMin
+					if rs > tmp {
+						rs = tmp
+					}
+				}
+			}
+		}
+	}
+	if rs == math.MaxInt {
+		return -1
+	}
+	return rs
+}
+
 func main() {
-	// fmt.Println(minimumSum([]int{5, 4, 8, 7, 10, 2})) // 13
-	fmt.Println(arithmeticTriplets([]int{0, 1, 4, 6, 7, 10}, 3))
+	fmt.Println(minimumSum([]int{6, 5, 4, 3, 4, 5}))  // -1
+	fmt.Println(minimumSum([]int{5, 4, 8, 7, 10, 2})) // 13
+	// fmt.Println(arithmeticTriplets([]int{0, 1, 4, 6, 7, 10}, 3))
 	// fmt.Println(maximumTripletValue([]int{12, 6, 1, 2, 7}))
 	// fmt.Println(mostPoints([][]int{{3, 2}, {4, 3}, {4, 4}, {2, 5}}))
 	// fmt.Println(climbStairs(3))
