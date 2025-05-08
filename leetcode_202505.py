efddafcd3b90eb8e770520754e26fcd4683ddedb
Nguyen Pham <phamhoannguyen@gmail.com>
2025-05-08 18:16:08 +0700

    3342

--- leetcode_202505.py
+++ leetcode_202505.py
@@ -58,6 +58,42 @@ def minTimeToReach(moveTime: list[list[int]]) -> int:
                     heapq.heappush(pq, (t, x, y))
 
 
+def minTimeToReach3342(moveTime: list[list[int]]) -> int:
+    # 3342
+    n, m = len(moveTime), len(moveTime[0])
+    dist = [[math.inf] * m for _ in range(n)]
+    dist[0][0] = 0
+
+    # Priority queue for Dijkstra's algorithm starting at top-left corner
+    pq = [(0, 0, 0)]
+
+    dirs = (-1, 0, 1, 0, -1)
+
+    while True:
+        d, i, j = heapq.heappop(pq)  # Pop the smallest distance node
+
+        # If we've reached the bottom-right corner, return the total distance
+        if i == n - 1 and j == m - 1:
+            return d
+
+        # Skip if we've found a shorter path to (i, j) already
+        if d > dist[i][j]:
+            continue
+
+        # Explore all 4 possible directions
+        for a, b in itertools.pairwise(dirs):
+            x, y = i + a, j + b
+            # Check if the new position is within bounds
+            if 0 <= x < n and 0 <= y < m:
+                # New time is the greater of either the given cell's moving time
+                # or the currently calculated time plus a constant based on parity of coordinates
+                t = max(moveTime[x][y], dist[i][j]) + (i + j) % 2 + 1
+                # If found a shorter path to (x, y), update distance and push to queue
+                if dist[x][y] > t:
+                    dist[x][y] = t
+                    heapq.heappush(pq, (t, x, y))
+
+
 assert minTimeToReach([[0, 4], [4, 4]]) == 6
 
 # assert romanToInt("LVIII") == 58
