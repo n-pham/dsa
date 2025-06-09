dbe930bd97c0838a886152cceaf420efeb007001
Nguyen Pham <phamhoannguyen@gmail.com>
2025-06-09 16:07:19 +0700

    35

--- lc202506/main.go
+++ lc202506/main.go
@@ -215,3 +215,19 @@ func findKthNumber(n int, k int) int {
 	}
 	return curr
 }
+
+func searchInsert(nums []int, target int) int {
+	// 35
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return left
+}
