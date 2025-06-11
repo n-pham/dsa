e97df68e8ded8a2166ee3420644bac21aeb27373
Nguyen Pham <phamhoannguyen@gmail.com>
2025-06-11 13:54:44 +0700

    3445 Python

--- leetcode_202506.py
+++ leetcode_202506.py
@@ -76,9 +76,43 @@ def clearStars(self, s: str) -> str:
     return "".join(char for i, char in enumerate(s) if not remove[i])
 
 
-assert (
-    maxCandies(
-        [1, 0, 1, 0], [7, 5, 4, 100], [[], [], [1], []], [[1, 2], [3], [], []], [0]
-    )
-    == 16
-)
+def maxDifference(s: str, k: int) -> int:
+    def get_status(a, b):
+        return ((a & 1) << 1) | (b & 1)
+
+    n = len(s)
+    ans = float("-inf")
+
+    for ca in range(5):
+        for cb in range(5):
+            if ca == cb:
+                continue
+            best = [float("inf")] * 4
+            cnt_a = cnt_b = prev_a = prev_b = 0
+            left = -1
+            for right in range(n):
+                cnt_a += s[right] == str(ca)
+                cnt_b += s[right] == str(cb)
+                while right - left >= k and cnt_b - prev_b >= 2:
+                    status = get_status(prev_a, prev_b)
+                    best[status] = min(best[status], prev_a - prev_b)
+                    left += 1
+                    prev_a += s[left] == str(ca)
+                    prev_b += s[left] == str(cb)
+                status = get_status(cnt_a, cnt_b)
+                if best[status ^ 2] < float("inf"):
+                    ans = max(ans, cnt_a - cnt_b - best[status ^ 2])
+
+    return ans
+
+
+assert maxDifference("1122211", 3) == 1
+assert maxDifference("12233", 4) == -1
+assert maxDifference("000112233444", 5) == 2
+
+# assert (
+#     maxCandies(
+#         [1, 0, 1, 0], [7, 5, 4, 100], [[], [], [1], []], [[1, 2], [3], [], []], [0]
+#     )
+#     == 16
+# )
