a95d99d651fca6cc598662a9d5c9c5440a9c1f1d
Nguyen Pham <phamhoannguyen@gmail.com>
2025-06-20 09:15:20 +0700

    3443

--- leetcode_202506.py
+++ leetcode_202506.py
@@ -116,6 +116,26 @@ def partitionArray(nums: list[int], k: int) -> int:
             mn = nums[i]
     return ans
 
+def maxDistance(self, s: str, k: int) -> int:
+    # 3443
+    return max(self._flip(s, k, 'NE'), self._flip(s, k, 'NW'),
+               self._flip(s, k, 'SE'), self._flip(s, k, 'SW'))
+
+def _flip(self, s: str, k: int, direction: str) -> int:
+    res = 0
+    pos = 0
+    opposite = 0
+
+    for c in s:
+        if c in direction:
+            pos += 1
+        else:
+            pos -= 1
+            opposite += 1
+        res = max(res, pos + 2 * min(k, opposite))
+
+    return res
+
 assert maxDifference("1122211", 3) == 1
 assert maxDifference("12233", 4) == -1
 assert maxDifference("000112233444", 5) == 2
