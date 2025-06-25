a355e275e408a65303ea419dc8d1739c18269e98
Nguyen Pham <phamhoannguyen@gmail.com>
2025-06-25 20:52:41 +0700

    263

--- lc202506/main.go
+++ lc202506/main.go
@@ -708,3 +708,17 @@ func AddDigits(num int) int {
 	}
 	return newNum
 }
+
+func IsUgly(n int) bool {
+	// 263
+	if n == 0 {
+		return false
+	}
+	for ; n%5 == 0; n /= 5 {
+	}
+	for ; n%3 == 0; n /= 3 {
+	}
+	for ; n%2 == 0; n /= 2 {
+	}
+	return n == 1
+}
