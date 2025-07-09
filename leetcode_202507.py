564ba82a8c9571ffa501a17d467dd4e8cf27e6d6
Nguyen Pham <phamhoannguyen@gmail.com>
2025-07-09 09:57:59 +0700

    3439 Python

--- leetcode_202507.py
+++ leetcode_202507.py
@@ -1,6 +1,8 @@
 import collections
 import heapq
 import sys
+import bisect
+import functools
 
 def possibleStringCount(word: str, k: int) -> int:
     # 3333
@@ -67,3 +69,46 @@ def maxEvents(events: list[list[int]]) -> int:
     
     # Return the total number of events attended
     return max_events_attended
+
+def maxValue(self, events: list[list[int]], k: int) -> int:
+    # 1751
+    n = len(events)
+    events.sort()
+
+    starts = [e[0] for e in events]
+
+    @functools.lru_cache(None)
+    def dp(i, k_left):
+        if k_left == 0 or i >= n:
+            return 0
+
+        # Option 1: Skip event i.
+        # This is the value if we don't attend event i.
+        res = dp(i + 1, k_left)
+
+        # Option 2: Attend event i.
+        # Find the index of the first event that starts after event i ends.
+        end_day = events[i][1]
+        next_i = bisect.bisect_right(starts, end_day)
+        
+        # The value is the current event's value + max value from non-overlapping
+        # future events, with one less event allowed to be attended.
+        res = max(res, events[i][2] + dp(next_i, k_left - 1))
+        
+        return res
+
+    return dp(0, k)
+
+def maxFreeTime(eventTime: int, k: int, startTime: list[int], endTime: list[int]) -> int:
+    gaps = ([startTime[0]] +
+            [startTime[i] - endTime[i - 1] for i in range(1, len(startTime))] +
+            [eventTime - endTime[-1]])
+    windowSum = sum(gaps[:k + 1])
+    ans = windowSum
+
+    for i in range(k + 1, len(gaps)):
+        windowSum += gaps[i] - gaps[i - k - 1]
+        ans = max(ans, windowSum)
+
+    return ans
+
