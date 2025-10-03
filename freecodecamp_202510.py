52bc3f2cd8fdca77896019117bf2ee2824aa01f1
Nguyen Pham <phamhoannguyen@gmail.com>
2025-10-03 14:15:21 +0700

    FCC 1003

--- freecodecamp_202510.py
+++ freecodecamp_202510.py
@@ -1,9 +1,47 @@
+def check_strength_1(password):
+    rules = [
+        lambda p: len(p) >= 8,
+        lambda p: any(char.isupper() for char in p)
+        and any(char.islower() for char in p),
+        lambda p: any(char.isdigit() for char in p),
+        lambda p: any(char in "!@#$%^&*" for char in p),
+    ]
+    meets = [1 if rule(password) else 0 for rule in rules]
+    return {4: "strong", 3: "medium", 2: "medium"}.get(sum(meets), "weak")
+
+
+def check_strength(password):
+    meets = {rule: 0 for rule in ["len", "both cases", "number", "special char"]}
+    if len(password) >= 8:
+        meets["len"] = 1
+    has_lower, has_upper = False, False
+    for char in password:
+        if char.islower():
+            has_lower = True
+        elif char.isupper():
+            has_upper = True
+        elif char.isdigit():
+            meets["number"] = 1
+        elif char in "!@#$%^&*":
+            meets["special char"] = 1
+    if has_lower and has_upper:
+        meets["both cases"] = 1
+    return {4: "strong", 3: "medium", 2: "medium"}.get(sum(meets.values()), "weak")
+
+
+assert check_strength("pass!!!") == "weak"
+assert check_strength("PassWord%^!") == "medium"
+assert check_strength("qwerty12345") == "medium"
+assert check_strength("S3cur3P@ssw0rd") == "strong"
+
+
 def to_binary(decimal):
     result = ""
     while decimal > 0:
         result = str(decimal % 2) + result
         decimal //= 2
     return result
+
 
 def to_decimal(binary):
     result = 0
