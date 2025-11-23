ce1c7aa78751ca8af212c7ae482409757b151596
Nguyen Pham <phamhoannguyen@gmail.com>
2025-11-23 08:17:12 +0700

    3190 Rust

---
+++ lc202511/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn minimum_operations(nums: &[i32]) -> usize {
+    // 3190
+    nums.iter()
+        .filter(|&&x| x % 3 != 0)
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_minimum_operations() {
+        let nums = vec![1, 3, 4, 6, 7, 9];
+        assert_eq!(minimum_operations(&nums), 3); // 1,4,7
+    }
+}
