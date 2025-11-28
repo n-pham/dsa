7c4f2577b844abbc0fef623733ce0e4ca1af703e
Nguyen Pham <phamhoannguyen@gmail.com>
2025-11-28 09:02:44 +0700

    50 2

--- 50_days_of_Python.py
+++ 50_days_of_Python.py
@@ -1,6 +1,23 @@
 import math
 
 
+def convert_add(strings: list[str]) -> int:
+    return sum([int(num_str) for num_str in strings])
+
+
+def test_convert_add():
+    assert convert_add(["1", "3", "5"]) == 9
+
+
+def check_duplicates(strings: list[str]) -> bool:
+    return len(set(strings)) == len(strings)
+
+
+def test_check_duplicates():
+    assert not check_duplicates(["apple", "orange", "banana", "apple"])
+    assert check_duplicates(["Yoda", "Moses", "Joshua", "Mark"])
+
+
 def first_longest_value(d: dict[str, str]) -> str:
     longest, longest_value = 0, ""
     for val in d.values():
