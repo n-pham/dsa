2810cd370734b46c7cca8552304f2bc5ef8e1085
Nguyen Pham <phamhoannguyen@gmail.com>
2026-03-03 19:17:45 +0700

    1545

--- lc202603/src/lib.rs
+++ lc202603/src/lib.rs
@@ -4,9 +4,29 @@ pub fn min_partitions(n: String) -> i32 {
     n.chars().map(|c| (c as u8 - b'0') as i32).max().unwrap_or(0)
 }
 
+pub fn find_kth_bit(n: i32, k: i32) -> char {
+    // 1545
+    let mut chars: Vec<char> = vec!['0'];
+    for _ in 1..n {
+        let mut part2: Vec<char> = chars.iter()
+            .rev()
+            .map(|&c| if c == '0' { '1' } else { '0' })
+            .collect();
+        chars.push('1');
+        chars.append(&mut part2);
+    }
+    chars[k as usize - 1]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_find_kth_bit() {
+        assert_eq!(find_kth_bit(3, 1), '0');
+        assert_eq!(find_kth_bit(4, 11), '1');
+    }
 
     #[test]
     fn test_min_partitions() {
