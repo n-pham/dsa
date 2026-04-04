7e4094796dd5ba240c5b13cb65797c605fc48d99
Nguyen Pham <phamhoannguyen@gmail.com>
2026-04-04 17:19:19 +0700

    2075

--- lc202604/src/lib.rs
+++ lc202604/src/lib.rs
@@ -1,3 +1,45 @@
 pub fn is_leap_year(year: u64) -> bool {
     year.is_multiple_of(400) || (year.is_multiple_of(4) && !year.is_multiple_of(100))
 }
+
+pub fn decode_ciphertext_mem(encoded_text: String, rows: i32) -> String {
+    let n = encoded_text.len();
+    let rows = rows as usize;
+    let cols = n / rows;
+    let chars: Vec<char> = encoded_text.chars().collect();
+    let mut result = String::new();
+    for c in 0..cols {
+        let mut row = 0;
+        let mut col = c;
+        // (0, c) -> (1, c+1) -> (2, c+2) ...
+        while row < rows && col < cols {
+            let index = row * cols + col;
+            result.push(chars[index]);
+            row += 1;
+            col += 1;
+        }
+    }
+    result.trim_end().to_string()
+}
+
+pub fn decode_ciphertext(encoded_text: String, rows: i32) -> String {
+    // 2075
+    let bytes = encoded_text.as_bytes(); // No collection/allocation!
+    let rows = rows as usize;
+    let cols = bytes.len() / rows;
+    let mut result = Vec::new();
+    for c in 0..cols {
+        let mut r = 0;
+        let mut current_col = c;
+        // (0, c) -> (1, c+1) -> (2, c+2) ...
+        while r < rows && current_col < cols {
+            let index = r * cols + current_col;
+            if index < bytes.len() {
+                result.push(bytes[index]);
+            }
+            r += 1;
+            current_col += 1;
+        }
+    }
+    String::from_utf8_lossy(&result).trim_end().to_string()
+}
