2a8c33a9e6c674108125b04b700bf7c59abea103
Nguyen Pham <phamhoannguyen@gmail.com>
2026-05-02 19:30:56 +0700

    788

--- lc202605/src/lib.rs
+++ lc202605/src/lib.rs
@@ -19,3 +19,21 @@ pub fn max_rotate_function(nums: Vec<i32>) -> i32 {
     }
     max_val as i32
 }
+
+pub fn rotated_digits(n: i32) -> i32 {
+    // 788
+    fn is_rotated_good(x: &i32) -> bool {
+        let mut tmp = *x;
+        let mut has_diff = false;
+        while tmp > 0 {
+            match tmp % 10 {
+                3 | 4 | 7 => return false,
+                2 | 5 | 6 | 9 => has_diff = true,
+                _ => {}
+            }
+            tmp /= 10
+        }
+        has_diff
+    }
+    (1..=n).filter(is_rotated_good).count() as i32
+}
