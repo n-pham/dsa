9909cd1ccd7822a4deac1679e74e7c903fd8aabe
Nguyen Pham <phamhoannguyen@gmail.com>
2026-05-19 17:34:57 +0700

    2540

--- lc202605/src/lib.rs
+++ lc202605/src/lib.rs
@@ -135,3 +135,19 @@ pub fn is_good(nums: Vec<i32>) -> bool {
     // The length must also match n + 1
     current_sum == expected_sum && nums.len() == (max_num as usize + 1)
 }
+
+pub fn get_common(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+    // 2540
+    let (len1, len2) = (nums1.len(), nums2.len());
+    let (mut i1, mut i2) = (0, 0);
+    while i1 < len1 && i2 < len2 {
+        if nums1[i1] == nums2[i2] {
+            return nums1[i1];
+        } else if nums1[i1] < nums2[i2] {
+            i1 += 1;
+        } else {
+            i2 += 1;
+        }
+    }
+    -1
+}
